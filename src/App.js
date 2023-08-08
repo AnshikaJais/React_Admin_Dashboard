17e3ed2d96eb953b01bb01c74545011a13113f35
AnshikaJais <86293614+AnshikaJais@users.noreply.github.com>
2023-08-08 14:05:14 +0530

    Final Commit.

--- src/App.js
+++ src/App.js
@@ -75,7 +75,6 @@ function App() {
                             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                                 <Navbar />
                             </div>
-
                             <div>
                                 {themeSettings && <ThemeSettings />}
                                 <Routes>
